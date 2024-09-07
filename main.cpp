0969b9a6cb9c1b110aa20c8a4546ff6e68d20d26
Reece-Cosbey <122653788+Reece-Cosbey@users.noreply.github.com>
2024-09-07 14:01:13 -0500

    Feature

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 
 using namespace std;
-
-int maint(){
-  cout <<"Hello World!";
+int sum(int n){
+  int i = 1;
+  int n2 = n;
+  while (i != n){
+    n2 = n2 + i;
+    i++;
+  }
+  return n2;
+}
+int product(int n){
+  int i = 1;
+  int n2 = n;
+  while (i != n){
+    n2 = n2*i;
+    i++;
+  }
+  return n2;
+}
+int main() {
+  int number;
+  cout << "Input a number ";
+  cin >> number;
+  int number2 = sum(number);
+  int number3 = product(number);
+  cout << number3 <<" " << number2 << endl;
   return 0;
 }
